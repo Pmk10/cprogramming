d8eba7f415dc4e66acf7324519cf1f5e1c30ebd0
Pmk10 <47448179+Pmk10@users.noreply.github.com>
2019-02-08 18:32:06 +0530

    Create program4.c

---
+++ program4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+char a[50]={'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
+char b;
+printf("Input \n");
+if(b==a[50])
+{
+	printf("Alphabet \n");
+}
+else
+printf("No \n");
+	return 0;
+}
